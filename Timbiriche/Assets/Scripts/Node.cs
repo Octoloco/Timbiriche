8ee5dd5198b88f29fb9fd8b4553774321ac6aa4f
Diego Clorio <diego_clorio@hotmail.com>
2022-03-15 02:42:03 -0600

    Finished grid generation and node assignment

---
+++ Timbiriche/Assets/Scripts/Node.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Transform NorthNode;
+    public Transform SouthNode;
+    public Transform WestNode;
+    public Transform EastNode;
+}
